afacf4ec702afbb83c0bc9bfee4a1b6cbb5a3a87
Krishnendhu-jills <krishnendhujills@gmail.com>
2025-07-31 15:27:48 +0530

    exercise 1

---
+++ nnnnnn.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c;
+  printf("enter 2 no.s ");
+  scanf("%d %d ",&a,&b);
+
+}
---
+++ sample.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a,b,c;
+  printf("enter 2 no.s ");
+  scanf("%d %d ",&a, &b);
+  c=a+b;
+  printf("the sum is %d",c);
+
+  return 0;
+}
